d1a286951660f60d885d4048f1b146ddb0c54be1
Nitish <mail.logicninjax@gmail.com>
2025-08-06 22:23:24 +0530

    add service method

--- src/main/java/com/app/parking/service/UserService.java
+++ src/main/java/com/app/parking/service/UserService.java
@@ -1,4 +1,10 @@
 package com.app.parking.service;
 
+import com.app.parking.dto.request.RegisterRequest;
+import com.app.parking.dto.response.RegisterResponse;
+import com.app.parking.exception.custom_exception.FieldUniqueException;
+
 public interface UserService {
+
+    RegisterResponse registerUser(RegisterRequest request) throws FieldUniqueException;
 }
