aef30b8a19c1a6b07e5f1f98a9090d11129ee493
Nitish <mail.logicninjax@gmail.com>
2025-08-06 22:24:43 +0530

    add auth controller

---
+++ src/main/java/com/app/parking/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.app.parking.controller;
+
+
+import com.app.parking.dto.request.RegisterRequest;
+import com.app.parking.dto.response.ApiResponse;
+import com.app.parking.dto.response.RegisterResponse;
+import com.app.parking.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(path = "/register", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<ApiResponse<RegisterResponse>> register(@RequestBody RegisterRequest request){
+        var response = userService.registerUser(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponse<>(true, "user registered successfully", response));
+    }
+}
