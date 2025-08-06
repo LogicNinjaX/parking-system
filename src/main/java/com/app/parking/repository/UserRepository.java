e02c778f7e48ff5e2e03bb5cc81ef2d233ad1c5b
Nitish <mail.logicninjax@gmail.com>
2025-08-06 23:04:38 +0530

    add find by method

--- src/main/java/com/app/parking/repository/UserRepository.java
+++ src/main/java/com/app/parking/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.app.parking.repository;
 
 import com.app.parking.entity.User;
+import com.app.parking.exception.custom_exception.UserNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username) throws UserNotFoundException;
 }
