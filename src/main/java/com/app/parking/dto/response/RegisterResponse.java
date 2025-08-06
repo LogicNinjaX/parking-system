d5f7a3995127a44c7e826abd88f930756b1661d3
Nitish <mail.logicninjax@gmail.com>
2025-08-06 21:34:08 +0530

    add getters/setters

--- src/main/java/com/app/parking/dto/response/RegisterResponse.java
+++ src/main/java/com/app/parking/dto/response/RegisterResponse.java
@@ -15,4 +15,44 @@ public class RegisterResponse {
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
