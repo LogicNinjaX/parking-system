de7baa128f44464ba3ce20f74ebd841f2830e25a
Nitish <mail.logicninjax@gmail.com>
2025-08-06 20:57:41 +0530

    add user service and impl class

---
+++ src/main/java/com/app/parking/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.app.parking.service.impl;
+
+import com.app.parking.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
