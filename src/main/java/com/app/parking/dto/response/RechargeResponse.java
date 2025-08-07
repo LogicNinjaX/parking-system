9865822e4f0a5ab03ac73b4081e5eab0bdc7d749
Nitish <mail.logicninjax@gmail.com>
2025-08-07 19:44:04 +0530

    add response dto

---
+++ src/main/java/com/app/parking/dto/response/RechargeResponse.java
@@ -0,0 +1,14 @@
+package com.app.parking.dto.response;
+
+
+public class RechargeResponse {
+    private Double updatedBalance;
+
+    public Double getUpdatedBalance() {
+        return updatedBalance;
+    }
+
+    public void setUpdatedBalance(Double updatedBalance) {
+        this.updatedBalance = updatedBalance;
+    }
+}
