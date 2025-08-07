6edbcc246988f288b64295738a1df15207eba608
Nitish <mail.logicninjax@gmail.com>
2025-08-07 19:42:29 +0530

    add login method

--- src/main/java/com/app/parking/controller/AuthController.java
+++ src/main/java/com/app/parking/controller/AuthController.java
@@ -1,25 +1,40 @@
 package com.app.parking.controller;
 
 
+import com.app.parking.dto.request.LoginRequest;
 import com.app.parking.dto.request.RegisterRequest;
 import com.app.parking.dto.response.ApiResponse;
 import com.app.parking.dto.response.RegisterResponse;
+import com.app.parking.entity.User;
 import com.app.parking.service.UserService;
+import com.app.parking.util.JwtTokenUtil;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenUtil jwtTokenUtil;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, AuthenticationManager authenticationManager, JwtTokenUtil jwtTokenUtil) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenUtil = jwtTokenUtil;
     }
 
     @PostMapping(path = "/register", consumes = "application/json", produces = "application/json")
@@ -28,5 +43,25 @@ public class AuthController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ApiResponse<>(true, "user registered successfully", response));
+    }
+
+
+    @PostMapping(value = "/login", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<ApiResponse<Map<String, String>>> login(@Valid @RequestBody LoginRequest request){
+        Authentication authentication =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        User user = userService.getUserByUsername(userDetails.getUsername());
+
+        String token = jwtTokenUtil.generateToken(userDetails, user.getUserId(), user.getRole().name());
+
+        return ResponseEntity.ok(new ApiResponse<>(
+                true,
+                "login successful"
+                , Collections.singletonMap("token", token)
+        ));
     }
 }
