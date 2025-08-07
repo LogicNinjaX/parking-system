d95ed68b6a5fd875e0ba95c18bdf8215e2c10ef0
Nitish <mail.logicninjax@gmail.com>
2025-08-07 19:40:51 +0530

    implement service method and update register method

--- src/main/java/com/app/parking/service/impl/UserServiceImpl.java
+++ src/main/java/com/app/parking/service/impl/UserServiceImpl.java
@@ -3,14 +3,18 @@ package com.app.parking.service.impl;
 import com.app.parking.dto.request.RegisterRequest;
 import com.app.parking.dto.response.RegisterResponse;
 import com.app.parking.entity.User;
+import com.app.parking.entity.Wallet;
 import com.app.parking.enums.UserRole;
 import com.app.parking.exception.custom_exception.FieldUniqueException;
 import com.app.parking.mapper.UserMapper;
 import com.app.parking.repository.UserRepository;
+import com.app.parking.repository.WalletRepository;
 import com.app.parking.service.UserService;
 import jakarta.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,11 +22,13 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
     private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -30,6 +36,8 @@ public class UserServiceImpl implements UserService {
         User user = userMapper.toUser(request);
         try {
             user.setRole(UserRole.USER);
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+            user.setWallet(new Wallet()); // assigning a new wallet
             user = userRepository.save(user);
         } catch (ConstraintViolationException e) {
             if (e.getMessage().contains("uk_user_table_username")) {
@@ -40,5 +48,10 @@ public class UserServiceImpl implements UserService {
         }
         LOGGER.info("User saved with user id: {} and username: {}", user.getUserId(), user.getUsername());
         return userMapper.toResponse(user);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("user not found"));
     }
 }
