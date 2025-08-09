906fc12431950f3d38439675c3071f8cd1f2e1c1
Nitish <mail.logicninjax@gmail.com>
2025-08-09 19:18:44 +0530

    add custom query method

--- src/main/java/com/app/parking/repository/BookingHistoryRepository.java
+++ src/main/java/com/app/parking/repository/BookingHistoryRepository.java
@@ -1,6 +1,8 @@
 package com.app.parking.repository;
 
 import com.app.parking.entity.BookingHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +12,7 @@ public interface BookingHistoryRepository extends JpaRepository<BookingHistory, 
 
     @Query("SELECT COUNT(bh) FROM BookingHistory bh WHERE bh.user.userId = :userId AND bh.parkingData.parkingId = :parkingId")
     int checkBooking(UUID userId, UUID parkingId);
+
+    @Query("SELECT bh FROM BookingHistory bh JOIN FETCH bh.parkingData WHERE bh.user.userId = :userId")
+    Page<BookingHistory> getBookingHistory(UUID userId, Pageable pageable);
 }
