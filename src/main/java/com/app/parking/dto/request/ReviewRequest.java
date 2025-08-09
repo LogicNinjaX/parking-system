53a3268bebdbcbaba5ca3fc557ab8fb42e380bbb
Nitish <mail.logicninjax@gmail.com>
2025-08-09 16:57:01 +0530

    add dto

---
+++ src/main/java/com/app/parking/dto/request/ReviewRequest.java
@@ -0,0 +1,23 @@
+package com.app.parking.dto.request;
+
+public class ReviewRequest {
+    private int rating;
+
+    private String comment;
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
