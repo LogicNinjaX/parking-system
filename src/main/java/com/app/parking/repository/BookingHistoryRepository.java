3c2056faf41239f1e398afec136b629b9bf2f829
Nitish <mail.logicninjax@gmail.com>
2025-08-09 18:03:35 +0530

    add custom query method

--- src/main/java/com/app/parking/repository/BookingHistoryRepository.java
+++ src/main/java/com/app/parking/repository/BookingHistoryRepository.java
@@ -2,8 +2,12 @@ package com.app.parking.repository;
 
 import com.app.parking.entity.BookingHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface BookingHistoryRepository extends JpaRepository<BookingHistory, UUID> {
+
+    @Query("SELECT COUNT(bh) FROM BookingHistory bh WHERE bh.user.userId = :userId AND bh.parkingData.parkingId = :parkingId")
+    int checkBooking(UUID userId, UUID parkingId);
 }
--- src/main/java/com/app/parking/repository/ReviewRepository.java
+++ src/main/java/com/app/parking/repository/ReviewRepository.java
@@ -2,8 +2,12 @@ package com.app.parking.repository;
 
 import com.app.parking.entity.ParkingReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<ParkingReview, UUID> {
+
+    @Query("SELECT COUNT(pr) FROM ParkingReview pr WHERE pr.user.userId = :userId AND pr.parking.parkingId = :parkingId")
+    int countReview(UUID userId, UUID parkingId);
 }
