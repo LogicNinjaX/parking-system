6adfa27471b337fc539651208d508b34aaf5043b
Nitish <mail.logicninjax@gmail.com>
2025-08-09 18:48:48 +0530

    add custom query method

--- src/main/java/com/app/parking/repository/ReviewRepository.java
+++ src/main/java/com/app/parking/repository/ReviewRepository.java
@@ -1,6 +1,9 @@
 package com.app.parking.repository;
 
+import com.app.parking.entity.ParkingData;
 import com.app.parking.entity.ParkingReview;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +13,7 @@ public interface ReviewRepository extends JpaRepository<ParkingReview, UUID> {
 
     @Query("SELECT COUNT(pr) FROM ParkingReview pr WHERE pr.user.userId = :userId AND pr.parking.parkingId = :parkingId")
     int countReview(UUID userId, UUID parkingId);
+
+    @Query("SELECT pr FROM ParkingReview pr JOIN FETCH pr.user WHERE pr.parking.parkingId = :parkingId")
+    Page<ParkingReview> findReviewsByParkingId(UUID parkingId, Pageable pageable);
 }
