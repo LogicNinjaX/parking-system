506dd3f653d52653209338ddf17044f35bf581ec
Nitish <mail.logicninjax@gmail.com>
2025-08-09 18:48:18 +0530

    add mapper method

--- src/main/java/com/app/parking/mapper/ReviewMapper.java
+++ src/main/java/com/app/parking/mapper/ReviewMapper.java
@@ -1,6 +1,7 @@
 package com.app.parking.mapper;
 
 import com.app.parking.dto.request.ReviewRequest;
+import com.app.parking.dto.response.ReviewDataResponse;
 import com.app.parking.entity.ParkingReview;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,4 +14,7 @@ public interface ReviewMapper {
     @Mapping(target = "parking", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     ParkingReview toReview(ReviewRequest request);
+
+    @Mapping(target = "username", source = "parkingReview.user.username")
+    ReviewDataResponse toReviewDataResponse(ParkingReview parkingReview);
 }
