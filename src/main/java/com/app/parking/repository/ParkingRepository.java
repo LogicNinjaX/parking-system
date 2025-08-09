30230f210b8bc22af4b390d1dd6e84cd6fb44d7a
Nitish <mail.logicninjax@gmail.com>
2025-08-09 10:21:17 +0530

    add custom query method

--- src/main/java/com/app/parking/repository/ParkingRepository.java
+++ src/main/java/com/app/parking/repository/ParkingRepository.java
@@ -1,6 +1,8 @@
 package com.app.parking.repository;
 
 import com.app.parking.entity.ParkingData;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,4 +13,7 @@ public interface ParkingRepository extends JpaRepository<ParkingData, UUID> {
 
     @Query("SELECT p FROM ParkingData p JOIN FETCH p.owner WHERE p.parkingId = :parkingId")
     Optional<ParkingData> getParkingWithOwner(UUID parkingId);
+
+    @Query("SELECT p FROM ParkingData p WHERE p.isBooked = false AND p.isDisabled = false")
+    Page<ParkingData> getAvailableParking(Pageable pageable);
 }
