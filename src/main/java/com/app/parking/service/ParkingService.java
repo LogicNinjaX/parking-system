16c4edef0b796b7403d8069c1ebfa8459f6e17a8
Nitish <mail.logicninjax@gmail.com>
2025-08-09 10:21:46 +0530

    add service method

--- src/main/java/com/app/parking/service/ParkingService.java
+++ src/main/java/com/app/parking/service/ParkingService.java
@@ -2,10 +2,17 @@ package com.app.parking.service;
 
 import com.app.parking.dto.request.ListingRequest;
 import com.app.parking.dto.response.ListingResponse;
+import com.app.parking.dto.response.ParkingDataResponse;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ParkingService {
 
     ListingResponse listParkingSpace(UUID userId, ListingRequest request);
+
+    List<ParkingDataResponse> getAllParkingSpots(Pageable pageable);
+
+    List<ParkingDataResponse> getAvailableParkingSpots(Pageable pageable);
 }
