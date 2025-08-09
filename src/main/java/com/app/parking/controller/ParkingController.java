b611d8f1e92fabe9c6c52c0bfd10f11389cf9003
Nitish <mail.logicninjax@gmail.com>
2025-08-09 10:22:53 +0530

    add parking controller

---
+++ src/main/java/com/app/parking/controller/ParkingController.java
@@ -0,0 +1,71 @@
+package com.app.parking.controller;
+
+import com.app.parking.dto.request.BookingRequest;
+import com.app.parking.dto.request.ListingRequest;
+import com.app.parking.dto.response.ApiResponse;
+import com.app.parking.dto.response.BookingResponse;
+import com.app.parking.dto.response.ListingResponse;
+import com.app.parking.dto.response.ParkingDataResponse;
+import com.app.parking.security.CustomUserDetails;
+import com.app.parking.service.BookingService;
+import com.app.parking.service.ParkingService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/parking")
+public class ParkingController {
+
+    private final ParkingService parkingService;
+    private final BookingService bookingService;
+
+    public ParkingController(ParkingService parkingService, BookingService bookingService) {
+        this.parkingService = parkingService;
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<ApiResponse<List<ParkingDataResponse>>> getParkingSpots(
+            @PageableDefault(sort = {"price"}, direction = Sort.Direction.ASC) Pageable pageable,
+            @RequestParam(required = false) boolean available
+    )
+    {
+        var parkingList = (available) ?
+                parkingService.getAvailableParkingSpots(pageable) : parkingService.getAllParkingSpots(pageable);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(true, "parking spots fetched successfully", parkingList));
+    }
+
+    @PostMapping(path = "/list", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<ApiResponse<ListingResponse>> listParkingSpot(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestBody ListingRequest request
+            )
+    {
+        var response = parkingService.listParkingSpace(user.getUserId(), request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ApiResponse<>(true, "parking spot saved successfully", response));
+    }
+
+    @PostMapping(path = "/book/{parking-id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<ApiResponse<BookingResponse>> bookParkingSpot(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @PathVariable("parking-id") UUID parkingId,
+            @RequestBody BookingRequest request
+    )
+    {
+        var response = bookingService.bookParking(user.getUserId(), parkingId, request);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(true, "parking spot booked successfully", response));
+    }
+}
