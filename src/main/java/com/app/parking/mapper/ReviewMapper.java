ff4db21bead5a831dea618eacbfaee3955701a8f
Nitish <mail.logicninjax@gmail.com>
2025-08-09 18:02:11 +0530

    add review mapper

---
+++ src/main/java/com/app/parking/mapper/ReviewMapper.java
@@ -0,0 +1,16 @@
+package com.app.parking.mapper;
+
+import com.app.parking.dto.request.ReviewRequest;
+import com.app.parking.entity.ParkingReview;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ReviewMapper {
+
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "reviewId", ignore = true)
+    @Mapping(target = "parking", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    ParkingReview toReview(ReviewRequest request);
+}
