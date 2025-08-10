2db1a54e018225fe66617081601c13bc9433da56
Nitish <mail.logicninjax@gmail.com>
2025-08-10 11:18:47 +0530

    add handler methods

--- src/main/java/com/app/parking/exception/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/com/app/parking/exception/exception_handler/GlobalExceptionHandler.java
@@ -2,7 +2,12 @@ package com.app.parking.exception.exception_handler;
 
 
 import com.app.parking.dto.response.ErrorResponse;
+import com.app.parking.exception.custom_exception.BalanceErrorException;
+import com.app.parking.exception.custom_exception.BookingFailedException;
+import com.app.parking.exception.custom_exception.FieldUniqueException;
+import com.app.parking.exception.custom_exception.ReviewExistException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -17,7 +22,7 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponse<?>> entityNotFoundErrorHandler(EntityNotFoundException e){
+    public ResponseEntity<ErrorResponse<String>> entityNotFoundErrorHandler(EntityNotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse<>(false, "entity not found", HttpStatus.NOT_FOUND.value(), e.getMessage()));
     }
@@ -38,10 +43,35 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse<?>> handleGenericException(Exception ex) {
+    public ResponseEntity<ErrorResponse<String>> handleGenericException(Exception ex) {
         ErrorResponse<String> error = new ErrorResponse<>(false, "Something went wrong: " + ex.getMessage(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value(), null);
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
     }
+
+    @ExceptionHandler(BookingFailedException.class)
+    public ResponseEntity<ErrorResponse<String>> bookingFailedHandler(BookingFailedException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new ErrorResponse<>(false, "booking failed", HttpStatus.CONFLICT.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(BalanceErrorException.class)
+    public ResponseEntity<ErrorResponse<String>> errorBalanceHandler(BalanceErrorException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse<>(false, "balance error", HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(exception = {FieldUniqueException.class, DataIntegrityViolationException.class})
+    public ResponseEntity<ErrorResponse<String>> fieldUniqueHandler(FieldUniqueException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new ErrorResponse<>(false, "duplicate entry", HttpStatus.CONFLICT.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler(ReviewExistException.class)
+    public ResponseEntity<ErrorResponse<String>> reviewExistError(ReviewExistException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new ErrorResponse<>(false, "review already exist", HttpStatus.CONFLICT.value(), e.getMessage()));
+    }
+
 }
