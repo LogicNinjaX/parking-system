27db70be6fac7e93f5147cabcebd5857968bd4bb
Nitish <mail.logicninjax@gmail.com>
2025-08-18 10:27:57 +0530

    add open api configuration

---
+++ src/main/java/com/app/parking/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.app.parking.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme ="bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("P2P Parking Api")
+                                .version("1.0")
+                                .description("Api Documentation for parking system")
+                );
+    }
+}
