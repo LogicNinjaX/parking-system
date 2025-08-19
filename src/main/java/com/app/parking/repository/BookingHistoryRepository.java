c7e05a0f8b1a84e280b1d0f9a5fed23b9b4af496
Nitish <mail.logicninjax@gmail.com>
2025-08-19 09:54:35 +0530

    add custom query method

--- src/main/java/com/app/parking/repository/BookingHistoryRepository.java
+++ src/main/java/com/app/parking/repository/BookingHistoryRepository.java
@@ -1,11 +1,14 @@
 package com.app.parking.repository;
 
 import com.app.parking.entity.BookingHistory;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface BookingHistoryRepository extends JpaRepository<BookingHistory, UUID> {
@@ -15,4 +18,9 @@ public interface BookingHistoryRepository extends JpaRepository<BookingHistory, 
 
     @Query("SELECT bh FROM BookingHistory bh JOIN FETCH bh.parkingData WHERE bh.user.userId = :userId")
     Page<BookingHistory> getBookingHistory(UUID userId, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE BookingHistory bh SET bh.parkingData.isBooked = false WHERE bh.parkingData.isBooked = true AND bh.endAt < :dateTime")
+    void updateStatus(LocalDateTime dateTime);
 }
