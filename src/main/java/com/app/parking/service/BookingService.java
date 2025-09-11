f41569ebca68b4ee12a7125398c98bc96940fa14
Nitish <mail.logicninjax@gmail.com>
2025-09-11 12:26:27 +0530

    add booking service method

--- src/main/java/com/app/parking/service/BookingService.java
+++ src/main/java/com/app/parking/service/BookingService.java
@@ -1,6 +1,7 @@
 package com.app.parking.service;
 
 import com.app.parking.dto.request.BookingRequest;
+import com.app.parking.dto.response.ApiResponse;
 import com.app.parking.dto.response.BookingResponse;
 import com.app.parking.exception.custom_exception.ParkingAlreadyBookedException;
 import com.app.parking.exception.custom_exception.ParkingNotFoundException;
@@ -11,4 +12,6 @@ import java.util.UUID;
 public interface BookingService {
 
     BookingResponse bookParking(UUID userId, UUID parkingId, BookingRequest request) throws ParkingNotFoundException, ParkingAlreadyBookedException, WalletNotFoundException;
+
+    ApiResponse<Void> cancelBooking(UUID userId, UUID parkingId);
 }
